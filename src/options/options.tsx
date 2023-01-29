dde94c427523fec59d9761e8fb56ae4ac42d8d5c
Antoine Aubert <antoine0aubert@edu.esiee.fr>
2023-01-29 17:53:51 +0100

    Part 6 options page is display

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -4,9 +4,7 @@ import '../assets/tailwind.css';
 
 const test = (
     <div>
-        <h1 className="text-5xl text-green-500">Hello World</h1>
-        <p>Lorem ipsum dolor sit amet...</p>
-        <p> Another test of the popup</p>
+        <h1 className="text-5xl text-green-500">Options</h1>
     </div>
 )
 
--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
         }),
         ...getHtmlPlugins([
             'popup',
-            'options',
+            'options'
         ])
     ],
     resolve: {
