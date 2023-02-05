8d57c142cdc518646a01f064dfa4dd7dd19fdcf5
Antoine Aubert <antoine0aubert@edu.esiee.fr>
2023-02-05 17:52:06 +0100

    contentScript.ts works

---
+++ dist/contentScript.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************************!*\
+  !*** ./src/contentScript/contentScript.ts ***!
+  \********************************************/
+window.onload = () => {
+    console.log("I am from content script")
+}
+/******/ })()
+;
+//# sourceMappingURL=contentScript.js.map
---
+++ src/contentScript/contentScript.ts
@@ -0,0 +1,3 @@
+window.onload = () => {
+    console.log("I am from content script")
+}
